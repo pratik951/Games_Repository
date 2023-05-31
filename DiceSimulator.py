e432c74f03413c14a72678064952c13ab7dc46b1
Your Name <you@example.com>
2023-05-31 22:08:43 +0100

    updated

--- DiceSimulator.py
+++ DiceSimulator.py
@@ -1,4 +1,29 @@
 import random
-nums = [1, 2, 3, 4, 5, 6]
-shuffled_nums = random.shuffle(nums)
-print(nums[1])
+
+def roll_dice():
+    """
+    Simulates rolling a six-sided dice.
+    """
+    return random.randint(1, 6)
+
+def main():
+    print("Welcome to the Dice Simulator Game!")
+    player_name = input("Enter your name: ")
+
+    while True:
+        print("\nOptions:")
+        print("1. Roll the dice")
+        print("2. Quit")
+        choice = input("Enter your choice (1 or 2): ")
+
+        if choice == "1":
+            dice_roll = roll_dice()
+            print(f"\n{player_name}, you rolled a {dice_roll}!")
+        elif choice == "2":
+            print("Thanks for playing!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
